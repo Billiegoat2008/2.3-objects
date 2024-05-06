4241dd143ad6867a598f896a7b5f9fec794f2608
Billiegoat2008 <156938498+Billiegoat2008@users.noreply.github.com>
2024-05-05 21:05:08 -0700

    Create carclass.java

---
+++ carclass.java
@@ -0,0 +1,9 @@
+package carclass;
+
+public class carclass {
+    public void printfunction() {
+        System.out.println("1999 Nissan skyline GT-R r-34");
+        System.out.println("1970 Dodge Charger R/T");
+        System.out.println("Mitsubishi eclipse spyder gts");
+    }
+}
