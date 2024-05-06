75eeda5975554e7bb932e767ec77a24254f9698f
Billiegoat2008 <156938498+Billiegoat2008@users.noreply.github.com>
2024-05-05 21:04:15 -0700

    Update and rename public class Horse.java to App.java

---
+++ App.java
@@ -0,0 +1,10 @@
+import carclass.carclass;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        carclass sw = new carclass();
+        sw.printfunction();
+
+    }
+}
